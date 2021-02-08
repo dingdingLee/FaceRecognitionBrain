ac4e2e55ecee19cb7dfcb588fe8540d1b58abfd1
dingdingLee <vickytory1015@gmail.com>
2021-02-09 00:58:35 +0900

    Face-Recognition-Brain

---
+++ src/component/Rank/Rank.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'dingdingLee, 당신의 현재 랭크는...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+        
+
+      
+
+    );
+}
+
+export default Rank;
